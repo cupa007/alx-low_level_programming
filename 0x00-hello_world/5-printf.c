f3ffd90156e1d201f76da26c3b26307d9220db7f
cupa007 <christian.ukoh@yahoo.com>
2022-09-09 10:45:41 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but te outcome is a piece of art,\n");
+	return (0);
+}
